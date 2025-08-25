d56b2c155a2d80f8f5d9ca8cf7c0955373a239e7
Samuel Calderon <samuelcalderon1202@hotmail.com>
2025-08-25 06:55:09 -0500

    shapely1

---
+++ src/shapely/shapely1.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+from shapely import Polygon
+
+poly = Polygon([(0,0),(0,1),(1,1),(0,1)])
+fig, ax = plt.subplots(figsize=(6,6))
+x,y = poly.exterior.xy
+
+ax.fill(x, y, color='blue', alpha=0.5, label='Polígono')
+ax.plot(x, y, color='black', linewidth=3, solid_capstyle='round')
+
+ax.set_title("Visualización de una geometría de Shapely")
+ax.set_aspect('equal', 'box')
+ax.legend()
+ax.grid(True)
+plt.show()
